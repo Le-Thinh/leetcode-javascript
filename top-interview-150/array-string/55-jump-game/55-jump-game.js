6a57d341340b85af3629441b10bd4927b9279cb5
Le-Thinh <lqt9112003@gmail.com>
2024-11-06 16:08:49 +0700

    can jump II

--- top-interview-150/array-string/55-jump-game/55-jump-game.js
+++ top-interview-150/array-string/55-jump-game/55-jump-game.js
@@ -5,27 +5,27 @@ and each element in the array represents your maximum jump length at that positi
 Return true if you can reach the last index, or false otherwise.
 */
 var canJump = (nums) => {
-    const memo = new Array(nums.length).fill(0);
-    memo[memo.length - 1] = 1;
-    
-    return canJumpFromIndex(nums, memo);
-}
+  const memo = new Array(nums.length).fill(0);
+  memo[memo.length - 1] = 1;
+
+  return canJumpFromIndex(nums, memo);
+};
 
 const canJumpFromIndex = (nums, memo, index = 0) => {
-    if (memo[index] !== 0) return memo[index] === 1;
+  if (memo[index] !== 0) return memo[index] === 1;
 
-    const furthestJump = Math.min(index + nums[index], nums.length - 1);
-    for (let nextIndex = (index + 1); nextIndex <= furthestJump; nextIndex++) {
-        if (!canJumpFromIndex(nums, memo, nextIndex)) continue
+  const furthestJump = Math.min(index + nums[index], nums.length - 1);
+  for (let nextIndex = index + 1; nextIndex <= furthestJump; nextIndex++) {
+    if (!canJumpFromIndex(nums, memo, nextIndex)) continue;
 
-        memo[index] = 1;
-        return true;
-    }
+    memo[index] = 1;
+    return true;
+  }
 
-    memo[index] = -1;
-    return false;
-}
+  memo[index] = -1;
+  return false;
+};
 
 const nums5 = [3, 2, 1, 0, 4];
 
-console.log('55 - Can Jump', canJump(nums5));
+console.log("55 - Can Jump", canJump(nums5));
